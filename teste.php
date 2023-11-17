ece9e954ecc8e64f5afd2358c87d19d2e60d3eb1
Paulo César <47919954+Msajt@users.noreply.github.com>
2023-11-17 19:03:37 -0300

    testando

--- teste.php
+++ teste.php
@@ -1,1 +1,17 @@
+<? php
 
+class FormInscricao{
+    //START TCPDF
+    include_once( 'vendor/autoload.php' );
+
+    $pdf = new TCPDF('P','mm','A4');
+    $pdf->SetPrintHeader(false);
+    $pdf->Open();
+    $pdf->AddPage();
+    $pdf->SetFont('times','',12)
+
+    //Gerar a primeira pagina do formulario
+
+    
+
+}
